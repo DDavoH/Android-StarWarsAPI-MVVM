34e432bdd4b22025ddadfeaefe3c0852c4353e6c
David Cruz Ramírez <davoh.dev@gmail.com>
2021-05-21 22:10:03 -0500

    repository package implemented

--- app/src/main/java/com/davoh/starwarsapi_mvvm/data/Repositories.kt
+++ app/src/main/java/com/davoh/starwarsapi_mvvm/data/Repositories.kt
@@ -1,4 +1,19 @@
 package com.davoh.starwarsapi_mvvm.data
 
-class Repositories {
+class CharacterRepository(
+    private val charactersDataSource: RemoteCharactersDataSource
+){
+    fun getCharacters(page:Int) = charactersDataSource.getCharacters(page)
+}
+
+class PlanetRepository(
+    private val planetsDataSource: RemotePlanetsDataSource
+){
+    fun getPlanets(page:Int) = planetsDataSource.getPlanets(page)
+}
+
+class VehicleRepository(
+    private val vehiclesDataSource: RemoteVehiclesDataSource
+){
+    fun getVehicles(page:Int) = vehiclesDataSource.getVehicles(page)
 }
--- app/src/main/java/com/davoh/starwarsapi_mvvm/data/di/RepositoryModule.kt
+++ app/src/main/java/com/davoh/starwarsapi_mvvm/data/di/RepositoryModule.kt
@@ -1,11 +1,25 @@
 package com.davoh.starwarsapi_mvvm.data.di
 
+import com.davoh.starwarsapi_mvvm.data.*
 import dagger.Module
 import dagger.Provides
 
 @Module
 class RepositoryModule {
 
-    //todo repositories here
+    @Provides
+    fun characterRepositoryProvider(
+        charactersDataSource: RemoteCharactersDataSource
+    ) = CharacterRepository(charactersDataSource)
+
+    @Provides
+    fun planetRepositoryProvider(
+        planetsDataSource: RemotePlanetsDataSource
+    )= PlanetRepository(planetsDataSource)
+
+    @Provides
+    fun vehicleRepositoryProvider(
+        vehicleDataSource: RemoteVehiclesDataSource
+    ) = VehicleRepository(vehicleDataSource)
 
 }
