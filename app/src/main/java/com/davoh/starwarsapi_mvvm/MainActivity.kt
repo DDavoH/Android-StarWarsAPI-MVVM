0443c5a817a750acf7287a73c9f4cd0337cd6d19
David Cruz Ramírez <davoh.dev@gmail.com>
2021-05-21 19:44:45 -0500

    viewBinding replaced with dataBinding

--- app/src/main/java/com/davoh/starwarsapi_mvvm/MainActivity.kt
+++ app/src/main/java/com/davoh/starwarsapi_mvvm/MainActivity.kt
@@ -6,10 +6,8 @@ import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import com.davoh.starwarsapi_mvvm.ui.main.SectionsPagerAdapter
 import com.davoh.starwarsapi_mvvm.databinding.ActivityMainBinding
+import com.davoh.starwarsapi_mvvm.ui.main.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,7 +15,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -26,11 +23,6 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = binding.tabs
         tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = binding.fab
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 }
